b31067910dc1289f5c8449b260e3e856dba0a1ae
rahul-mitra <rahul1991mira@gmail.com>
2021-05-08 20:10:04 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -45,25 +45,45 @@ var upload = multer({
 });
 
 
-const options: CorsOptions = {
-    allowedHeaders: [
-        'Origin',
-        'X-Requested-With',
-        'Content-Type',
-        'Accept',
-        'X-Access-Token'
-    ],
-    credentials: true,
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    origin: "*",
-    preflightContinue: false,
-};
+// const options: CorsOptions = {
+//     allowedHeaders: [
+//         'Origin',
+//         'X-Requested-With',
+//         'Content-Type',
+//         'Accept',
+//         'X-Access-Token'
+//     ],
+    
+//     credentials: true,
+//     methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+//     origin: "*",
+//     preflightContinue: false
+    
+// };
 
 const MongoConnected = MongooseDBConnection(DBconnectionString);
 
 
 const app = express();
-app.use(cors(options))
+// app.use(cors(options))
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', "true");
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: 1024 * 1024, type: "*" }));
 app.use(express.static('public'))
